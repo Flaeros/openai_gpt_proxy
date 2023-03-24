8c81fc218b5efda888197fa8a98690f109f9d58b
Aleksandr Birin <flaeross@gmail.com>
2023-03-24 16:12:33 +0100

    add block list, logging

--- main.py
+++ main.py
@@ -3,30 +3,62 @@ from collections import defaultdict
 import telebot
 from dotenv import load_dotenv
 from my_openai import *
+import logging
+
+logging.getLogger(__name__)
+logging.basicConfig(filename='log.log',
+                    filemode='a',
+                    format='%(asctime)s,%(msecs)d %(name)s %(levelname)s %(message)s',
+                    datefmt='%H:%M:%S',
+                    level=logging.WARNING)
 
 BASEDIR = os.path.abspath(os.path.dirname(__file__))
 load_dotenv(os.path.join(BASEDIR, 'tokens.env'))
 
 BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
 AI_TOKEN = os.getenv("CHAT_GPT_API_KEY")
+BLOCK_LIST = os.getenv("BLOCK_LIST")
 
 bot = telebot.TeleBot(BOT_TOKEN)
 openai.api_key = AI_TOKEN
 
 conversations = defaultdict(list)
 
+logging.warning(f'Starting application')
+
+def blocked(message):
+    if str(message.from_user.id) in BLOCK_LIST:
+        logging.warning(f'Blocked user access {message.from_user.first_name} {message.from_user.id}')
+        bot.reply_to(message, text='Вы в черном списке')
+        return True
+
+    return False
+
+
 @bot.message_handler(commands=['bot'])
 def command_message(message):
+    logging.warning(f'Chat message from {message.from_user.first_name} {message.from_user.id} ')
+    if blocked(message):
+        return
+
     respond(message)
 
 
 @bot.message_handler(commands=['help'])
 def command_message(message):
+    logging.warning(f'Help message from {message.from_user.first_name} {message.from_user.id} ')
+    if blocked(message):
+        return
+
     bot.send_message(message.chat.id, text='/bot или реплай задать вопрос. /clear очистить контекст')
 
 
 @bot.message_handler(commands=['clean', 'clear'])
 def command_message(message):
+    logging.warning(f'Cleaning message from {message.from_user.first_name} {message.from_user.id}')
+    if blocked(message):
+        return
+
     key = f'{message.chat.type}|{message.chat.id}'
     del conversations[key]
     bot.send_message(message.chat.id, text='Контекст очищен')
@@ -34,6 +66,10 @@ def command_message(message):
 
 @bot.message_handler(func=lambda message: True)
 def echo_all(message):
+    logging.warning(f'Private message from {message.from_user.first_name} {message.from_user.id}')
+    if blocked(message):
+        return
+
     respond(message)
 
 
